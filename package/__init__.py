4cbc247e4080242622d12f49be336a5cdcfd6190
Xiaoyu Su <xiaoyusxy19@gmail.com>
2021-08-19 17:09:30 -0600

    fafaf

--- package/__init__.py
+++ package/__init__.py
@@ -1,11 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask
+from package.config import Config
 
 app = Flask(__name__)
-# app.config.from_object(Config)
+app.config.from_object(Config)
 
 
 # # setup Blueprints
-# from package.user.routes import user
+from package.user.routes import user
 from package.main.routes import main
-# app.register_blueprint(user)
+app.register_blueprint(user)
 app.register_blueprint(main)
