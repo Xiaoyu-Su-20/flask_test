b5cd2b145a81bb70f125dfa6eb0364658fb877f8
Xiaoyu Su <xiaoyusxy19@gmail.com>
2021-08-19 17:04:05 -0600

    blueprint

--- package/__init__.py
+++ package/__init__.py
@@ -3,12 +3,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 # app.config.from_object(Config)
 
-@app.route("/")
-def home():
-    return render_template("home.html")
 
 # # setup Blueprints
 # from package.user.routes import user
-# from package.main.routes import main
+from package.main.routes import main
 # app.register_blueprint(user)
-# app.register_blueprint(main)
+app.register_blueprint(main)
