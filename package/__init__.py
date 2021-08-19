a8b513fbbc3580978c9166e4fb12138402bd85c6
Xiaoyu Su <xiaoyusxy19@gmail.com>
2021-08-19 16:59:15 -0600

    change app

--- package/__init__.py
+++ package/__init__.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 # app.config.from_object(Config)
 
-# setup Blueprints
-from package.user.routes import user
-from package.main.routes import main
-app.register_blueprint(user)
-app.register_blueprint(main)
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+# # setup Blueprints
+# from package.user.routes import user
+# from package.main.routes import main
+# app.register_blueprint(user)
+# app.register_blueprint(main)
