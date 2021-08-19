5490f89d63d301610f29f44774e8ce3a6d9903fc
Xiaoyu Su <xiaoyusxy19@gmail.com>
2021-08-19 16:13:22 -0600

    afff

--- package/config.py
+++ package/config.py
@@ -1,7 +1,4 @@
-import os
-
-
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_SQLSERVER")
+    SECRET_KEY = "hello"
+    SQLALCHEMY_DATABASE_URI = "mssql+pyodbc://rcAprimaDB:YAKBmWXTU8#2@rcaprimadb.database.windows.net/RC-BI-Data?driver=ODBC Driver 17 for SQL Server"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
